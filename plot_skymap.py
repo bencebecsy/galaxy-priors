770a2558908fa2d3b0bdb6c1dc9f4d159294f99f
Bence Bécsy <bence.becsy@ligo.org>
2017-07-26 14:40:04 -0400

    Added file.

---
+++ plot_skymap.py
@@ -0,0 +1,11 @@
+import numpy as np
+import healpy as hp
+import matplotlib.pyplot as plt
+
+DPI=300
+
+m = hp.read_map("skymap_prior.fits")
+hp.mollview(m, title="Mollview image RING")
+#hp.mollview(m, coord=['C','G'], title="Mollview image RING")
+hp.graticule()
+plt.savefig('skymap_prior.png', dpi=DPI)
