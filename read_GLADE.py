0ead7deb230fdc4c69770b16fe57cd6880a0f81e
Bence Becsy <bence.becsy@ldas-pcdev1.ligo.caltech.edu>
2017-07-28 05:50:58 -0700

    Optimized file saving.

--- read_GLADE.py
+++ read_GLADE.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 import numpy as np
 from timeit import default_timer as timer
-import json
+#import h5py
 
 start = timer()
 
@@ -14,6 +14,7 @@ dt2=0
 dt3=0
 
 itera=0
+num=0
 with open("GLADE_2.txt", 'r') as glade_file:
     for line in glade_file:
         time1 = timer()
@@ -26,13 +27,14 @@ with open("GLADE_2.txt", 'r') as glade_file:
         #        (16) H_err (17) apparent K mag (18) K_err (19) flag2 	0: no measured dist or z 1: measured z 2: measured dist
         #        (20) flag3 0: measured B mag 1: ML estimated B mag (21) B_err_min (22) B_err_max
         #        (23) flag4 0: z from measured dist 1: ML estimated z (24) z_err_min (25) z_err_max
-        #L = L[0:6] + [float(i) for i in L[6:19]] + [int(L[19])] + [int(L[20])] + [float(L[21])] + [float(L[22])] + [int(L[23])] + [float(L[24])] + [float(L[25])]
-        L = L[0:6] + [float(i) for i in L[6:19]] + [int(L[19])] # shorter version to save memory (last 6 column lesf out)
+        L = L[0:6] + [float(i) for i in L[6:19]] + [int(L[19])] + [int(L[20])] + [float(L[21])] + [float(L[22])] + [int(L[23])] + [float(L[24])] + [float(L[25])]
+        #L = L[0:6] + [float(i) for i in L[6:19]] + [int(L[19])] # shorter version to save memory (last 6 column lesf out)
         time3=timer()
         #if (L[8]>0.0 and L[8]<100.0):
         #if (L[8]<100.0 and L[8]>0.0):
         if True:
             glade += L
+            num += 1
         time4=timer()
         #print glade
         itera+=1
@@ -44,19 +46,17 @@ with open("GLADE_2.txt", 'r') as glade_file:
             print dt1, dt2, dt3
         
 glade = np.array(glade)
-glade = np.reshape(glade, (-1,26))
+glade = np.reshape(glade, (num,26))
 
 print glade
 print glade.shape
 
-glade.tofile("gx_list_glade.bin")
+np.save("gx_list_glade.npy", glade)
 
-#glade = glade.tolist()
-#
-#with open("gx_list_glade.json",'w') as outfile:
-#    json.dump(glade,outfile)
+#h5f = h5py.File('gx_list_glade.hdf5', 'w')
+#h5f.create_dataset('dataset_1', data=glade)
+#h5f.close()
 
-#np.save("gx_list_glade", glade)
 
 end = timer()
 print "Elapsed time: %r seconds" %(end-start)
