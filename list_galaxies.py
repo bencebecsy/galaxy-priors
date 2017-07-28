b52ea2e1368633926c0b7203866625a852cd4a49
Bence Becsy <bence.becsy@ldas-pcdev1.ligo.caltech.edu>
2017-07-28 08:01:09 -0700

    Initial commit.

---
+++ list_galaxies.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+import numpy as np
+import sys
+
+infile = sys.argv[1]
+
+a = np.load(infile)
+
+for i in range(a.size/26):
+    #print "%s %s %s" %(a[i][1], a[i][2], a[i][11])#B
+    #print "%s %s" %(a[i][3], a[i][13])#J
+    print "%s %s" %(a[i][3], a[i][15])#H
