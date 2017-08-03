77b9f9e40eda3251639a70479544d278996ca37c
bencebecsy <becsybence@caesar.elte.hu>
2017-08-03 14:57:58 -0700

    Changed to plot multiple lines from different skymaps onto the same plot

--- prob-area_chist.py
+++ prob-area_chist.py
@@ -6,24 +6,26 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import sys
 
-infile = sys.argv[1]
-outfile = "prob_chist_" + infile[9:-4] + "png"
-print outfile
+infiles = sys.argv[1:]
 
 DPI=300
 
-m = hp.read_map(infile)
+for infile in infiles:
+    outfile = "prob_chist_" + infile[9:-27] + ".png"
+    #print outfile
 
-sorted_m = np.sort(m)[::-1]*4*np.pi/m.size
-m_chist = np.cumsum(sorted_m)
+    m = hp.read_map(infile)
+    sorted_m = np.sort(m)[::-1]*4*np.pi/m.size
+    m_chist = np.cumsum(sorted_m)
+    #print sorted_m, m_chist
+    area = np.arange(1,(m.size+1)) / float(sorted_m.size) * 4*np.pi *180**2/np.pi**2
 
-print sorted_m, m_chist
-area = np.arange(1,(m.size+1)) / float(sorted_m.size) * 4*np.pi *180**2/np.pi**2
+    plt.plot(area,m_chist, lw=1, label=infile[-26:-5])
 
-plt.plot(area,m_chist, lw=1)
 plt.xlabel("Sky area [deg^2]")
 plt.ylabel("Probability within sky area")
 plt.gca().set_xscale('log')
 plt.gca().set_yscale('log')
 plt.grid()
+plt.legend()
 plt.savefig(outfile, dpi=DPI)
