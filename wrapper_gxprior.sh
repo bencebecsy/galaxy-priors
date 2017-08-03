47c27a8d9f822c2bb6697a392ec06298e37cceb5
bencebecsy <becsybence@caesar.elte.hu>
2017-08-03 16:13:15 -0700

    Changed parameters

--- wrapper_gxprior.sh
+++ wrapper_gxprior.sh
@@ -2,24 +2,35 @@
 
 
 generate_skymap_prior.py gx_list_200Mpc_filtered.npy 64 lum 1 0 0 0
-plot_skymap.py gx_prior_0.0-200.0Mpc_NSIDE64_w-lumB1.0J0.0H0.0K0.0.fits 1
-prob-area_chist.py gx_prior_0.0-200.0Mpc_NSIDE64_w-lumB1.0J0.0H0.0K0.0.fits
+plot_skymap.py gx_prior_0.0-200.0Mpc_NSIDE64_w-lumB1.0J0.0H0.0K0.0.fits 0
+
+generate_skymap_prior.py gx_list_200Mpc_filtered.npy 64 lum 2 0 0 0
+plot_skymap.py gx_prior_0.0-200.0Mpc_NSIDE64_w-lumB2.0J0.0H0.0K0.0.fits 0
+
+generate_skymap_prior.py gx_list_200Mpc_filtered.npy 64 lum 3 0 0 0
+plot_skymap.py gx_prior_0.0-200.0Mpc_NSIDE64_w-lumB3.0J0.0H0.0K0.0.fits 0
+
+generate_skymap_prior.py gx_list_200Mpc_filtered.npy 64 lum 4 0 0 0
+plot_skymap.py gx_prior_0.0-200.0Mpc_NSIDE64_w-lumB4.0J0.0H0.0K0.0.fits 0
+
+
+
+#generate_skymap_prior.py gx_list_200Mpc_filtered.npy 64 mag 1 0 0 0
+#plot_skymap.py gx_prior_0.0-200.0Mpc_NSIDE64_w-magB1.0J0.0H0.0K0.0.fits 0
+
+
 
 #generate_skymap_prior.py gx_list_200Mpc_filtered.npy 64 lum 0 1 0 0
 #plot_skymap.py gx_prior_0.0-200.0Mpc_NSIDE64_w-lumB0.0J1.0H0.0K0.0.fits 0
-#prob-area_chist.py gx_prior_0.0-200.0Mpc_NSIDE64_w-lumB0.0J1.0H0.0K0.0.fts
 
 #generate_skymap_prior.py gx_list_200Mpc_filtered.npy 64 lum 0 0 1 0
 #plot_skymap.py gx_prior_0.0-200.0Mpc_NSIDE64_w-lumB0.0J0.0H1.0K0.0.fits 0
-#prob-area_chist.py gx_prior_0.0-200.0Mpc_NSIDE64_w-lumB0.0J0.0H1.0K0.0.fits
 
 #generate_skymap_prior.py gx_list_200Mpc_filtered.npy 64 lum 0 0 0 1
 #plot_skymap.py gx_prior_0.0-200.0Mpc_NSIDE64_w-lumB0.0J0.0H0.0K1.0.fits 0
-#prob-area_chist.py gx_prior_0.0-200.0Mpc_NSIDE64_w-lumB0.0J0.0H0.0K1.0.fits
 
 #generate_skymap_prior.py gx_list_200Mpc_filtered.npy 64 lum 1 1 1 1
 #plot_skymap.py gx_prior_0.0-200.0Mpc_NSIDE64_w-lumB1.0J1.0H1.0K1.0.fits 0
-#prob-area_chist.py gx_prior_0.0-200.0Mpc_NSIDE64_w-lumB1.0J1.0H1.0K1.0.fits
 
 
 
